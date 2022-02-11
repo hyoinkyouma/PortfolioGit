d341d40480aff1ee79728bf1f162775c1dcc8656
hyoinkyouma <98786537+hyoinkyouma@users.noreply.github.com>
2022-02-11 23:31:52 +0800

    added more styling on win, added functionality to again button

--- games/script.js
+++ games/script.js
@@ -11,20 +11,26 @@ document.querySelector(".score").textContent = 50;
 document.querySelector(".guess").value;
 document.querySelector(".guess").value = 23;
 Even listner*/
-const number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 20) + 1;
 console.log(number);
 let score = 9;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
-
+  //No input
   if (!guess) {
     document.querySelector(".message").textContent = "Enter a number! 🤔";
-  } else if (guess === number) {
+  } //player wins
+  else if (guess === number) {
     document.querySelector(".message").textContent = "Correct Number! 🎉";
     document.querySelector(".number").textContent = number;
-  } else if (guess > number) {
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    var highscore = score > highscore ? (highscore = score) : (score = score);
+    document.querySelector(".highscore").textContent = highscore + 1;
+  } //Guess too high
+  else if (guess > number) {
     if (score > 0) {
       document.querySelector(".message").textContent = "Too High! 📈";
       document.querySelector(".score").textContent = score--;
@@ -32,6 +38,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You Lost 😓";
       document.querySelector(".score").textContent = 0;
     }
+    //Guess too low
   } else if (guess < number) {
     if (score > 0) {
       document.querySelector(".message").textContent = "Too Low! 📉";
@@ -41,4 +48,14 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  number = Math.trunc(Math.random() * 20) + 1;
+  console.log(number);
+  document.querySelector(".message").textContent = "Here we go again...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".guess").textContent = "0";
 });
