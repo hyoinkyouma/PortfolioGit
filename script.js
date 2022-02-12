a4e01b4f36ddc5f02c71fe1b4410abc844b5d1fe
hyoinkyouma <98786537+hyoinkyouma@users.noreply.github.com>
2022-02-13 05:44:09 +0800

    added stuff

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-"use strict;";
-const modal = document.querySelector(".modal");
+"use strict";
+const modal = document.querySelectorAll(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnOpenModal = document.querySelector(".open-modal");
+const btnCloseModal = document.querySelectorAll(".close-modal");
+const btnOpenModal = document.querySelectorAll(".open-modal");
 
 //Functions
-const close = function close() {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-  console.log("modal closed");
-};
-const open = function open() {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-  console.log("modal open");
-};
 
 //query selector only selects first one when multiple elements have same class
 // can use for loop to select individual buttons
 
 //EvenListeners
-btnOpenModal.addEventListener("click", open);
-btnCloseModal.addEventListener("click", close);
-overlay.addEventListener("click", close);
-document.addEventListener("keydown", function (e) {
-  //console.log(e);
-  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
-    close();
-  }
-});
+for (let i = 0; i < btnOpenModal.length; i++) {
+  const close = function close() {
+    modal[i].classList.add("hidden");
+    overlay.classList.add("hidden");
+    console.log("modal closed");
+  };
+  btnOpenModal[i].addEventListener("click", function open() {
+    modal[i].classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    console.log("modal open");
+  });
+  btnCloseModal[i].addEventListener("click", close);
+  overlay.addEventListener("click", close);
+}
