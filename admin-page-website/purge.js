55a6942a42c20cb80407c5a1fe531e1fc5c0a407
kyouma <roman.cabalum@gmail.com>
2023-09-30 20:37:03 +0800

    added reset keypair feature

---
+++ admin-page-website/purge.js
@@ -0,0 +1,19 @@
+const purgeBtn = document.getElementById("purge-link")
+
+purgeBtn.onclick = () => {
+    if (confirm("This will log out all users then purge rsa key pairs.")) {
+    try{
+        fetch("https://api.romanaugusto.tk/v1/regenerate-keys").then((e) => e.json()).then(data => {
+            if (data.result_success){
+                alert("Key pair successfully purged.")
+                location.reload()
+            } else {
+                alert("Failed to renew key pair")
+                location.reload()
+            }
+        })
+    } catch(e) {
+        alert("Internal Server Error.")
+    }
+    }
+}
